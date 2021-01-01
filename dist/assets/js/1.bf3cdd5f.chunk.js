cef6846acc6aeedd5ddf9a2db5928ed127911314
thassan <sohayb.muhammed@capgemini.com>
2021-01-01 14:18:59 +0100

    build

---
+++ dist/assets/js/1.bf3cdd5f.chunk.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[1],{1545:function(n,w,o){}}]);
---
+++ dist/assets/js/6.bf3cdd5f.chunk.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[6],{1550:function(n,w,o){}}]);
