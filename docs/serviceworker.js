2723151e022ceecdde7fecc546dff87bdfc75a79
thassan <sohayb.muhammed@capgemini.com>
2021-01-03 23:22:00 +0100

    scope change

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -25,7 +25,14 @@ self.addEventListener('fetch', (event) => {
             })
     )
 });
-
+navigator.serviceWorker.register('/static/mod/practice/service-worker.js', { scope: '/' })
+        .then(function (registration)
+        {
+          console.log('Service worker registered successfully');
+        }).catch(function (e)
+        {
+          console.error('Error during service worker registration:', e);
+        });
 // Activate the SW
 self.addEventListener('activate', (event) => {
     const cacheWhitelist = [];
